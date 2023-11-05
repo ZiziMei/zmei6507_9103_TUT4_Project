c25a22c8202d7b0a7bd394908e30eed7a4ea12e7
Zizi Mei <zmei6507@uni.sydney.edu.au>
2023-11-05 19:20:35 +1100

    I updated the sketch

--- sketch.js
+++ sketch.js
@@ -1,60 +1,54 @@
-var points = [];
-var mult = 0.005;
-var allimages=[];
-var pic;
-var pos = 0;
-var bg;
-var imgur;
-
-
-function preload(){
-  allimages=['assets/Claude_Monet,_Saint-Georges_majeur_au_crépuscule(1).jpg'];
-  pic = loadImage(allimages[pos]);
-}
-
 function setup() {
-  var ww= windowWidth;
-  createCanvas(windowWidth,windowHeight);
-  background(255,100);
-  imageMode(CORNER);
-
+  createCanvas(400, 400, WEBGL);
   angleMode(DEGREES);
-  noiseDetail(10);
-  inti();
-  push();
-
-}
-
-function inti(){
-
-  var rowPoints = 120;  //Amout
-  var distPoints = width/rowPoints;
-  for(var x=0; x<width;x+=distPoints){
-    for(var y=0; y<height;y+=distPoints){
-        var p = createVector(x+random(10,30),y+random(5,30));
-        points.push(p);
-        }
-    
-  }
-
 }
 
 function draw() {
+  background(30);
+  rotateX(85);
 
-   //background(255,10); //Background or
-   for(var i=0; i<points.length;i++){
-     
-     var angle = map(noise(points[i].x*mult,points[i].y*mult), 0,1,0,500); //Angle
-     points[i].add(createVector(cos(angle),sin(angle)));
+  translate(0, 0, -40);
+  for (var i = 0; i < 100; i++) {
+    beginShape();
+    for (var j = 0; j < 360; j += 10) {
+      var rad = i * 8;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var d = dist(0, 0, x, y);
+      var interpColor;
+      if (d <= 98) {
+        interpColor = lerpColor(color(196, 99, 85), color(189,120,51), map(d, 0, 98, 0, 1));
+      } else {
+        interpColor = lerpColor(color(209,134,61), color(88,142,189), map(d, 98.1, 200, 0, 1));
+      }
+      stroke(interpColor);
+      strokeWeight(2)
+      vertex(x, y);
+      noFill();
+    }
+    endShape(CLOSE);
+  }
 
-     circle(points[i].x,points[i].y,2);
-     //rect(points[i].x,points[i].y,3,5); //Shape Rectangle or Circle
-     let color = pic.get(points[i].x,points[i].y);
-     fill(color);
-     noStroke();
-   }
-
-  console.log(pos);
-
+  translate(0, 0, 100);
+  rotateX(7);
+  for (var i = 0; i < 100; i++) {
+    beginShape();
+    for (var j = 0; j < 360; j += 10) {
+      var rad = i * 8;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var d = dist(0, 0, x, y);
+      var interpColor;
+      if (d <= 66.66) {
+        interpColor = lerpColor(color(196, 99, 85), color(220, 147, 47), map(d, 0, 66.66, 0, 1));
+      } else {
+        interpColor = lerpColor(color(220, 147, 47), color(69,106,162), map(d, 66.67, 200, 0, 1));
+      }
+      stroke(interpColor);
+      strokeWeight(2)
+      vertex(x, y);
+      noFill();
+    }
+    endShape(CLOSE);
+  }
 }
-
