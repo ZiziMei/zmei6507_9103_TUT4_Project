933b61cfa9ba2956aee883f574a0b9bfeac879b9
Zizi Mei <zmei6507@uni.sydney.edu.au>
2023-11-05 19:24:30 +1100

    I updated the sketch

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
 }
 
@@ -11,15 +11,15 @@ function draw() {
   for (var i = 0; i < 100; i++) {
     beginShape();
     for (var j = 0; j < 360; j += 10) {
-      var rad = i * 8;
+      var rad = i * 5;
       var x = rad * cos(j);
       var y = rad * sin(j);
       var d = dist(0, 0, x, y);
       var interpColor;
-      if (d <= 98) {
-        interpColor = lerpColor(color(196, 99, 85), color(189,120,51), map(d, 0, 98, 0, 1));
+      if (d <= 200) {
+        interpColor = lerpColor(color(196, 99, 85), color(189,120,51), map(d, 0, 200, 0, 1));
       } else {
-        interpColor = lerpColor(color(209,134,61), color(88,142,189), map(d, 98.1, 200, 0, 1));
+        interpColor = lerpColor(color(209,134,61), color(88,142,189), map(d, 200.1, 500, 0, 1));
       }
       stroke(interpColor);
       strokeWeight(2)
@@ -29,26 +29,31 @@ function draw() {
     endShape(CLOSE);
   }
 
-  translate(0, 0, 100);
-  rotateX(7);
-  for (var i = 0; i < 100; i++) {
-    beginShape();
-    for (var j = 0; j < 360; j += 10) {
-      var rad = i * 8;
-      var x = rad * cos(j);
-      var y = rad * sin(j);
-      var d = dist(0, 0, x, y);
-      var interpColor;
-      if (d <= 66.66) {
-        interpColor = lerpColor(color(196, 99, 85), color(220, 147, 47), map(d, 0, 66.66, 0, 1));
-      } else {
-        interpColor = lerpColor(color(220, 147, 47), color(69,106,162), map(d, 66.67, 200, 0, 1));
-      }
-      stroke(interpColor);
-      strokeWeight(2)
-      vertex(x, y);
-      noFill();
+translate(0, 0, 100);
+rotateX(7);
+for (var i = 0; i < 100; i++) {
+  beginShape();
+  for (var j = 0; j < 360; j += 10) {
+    var rad = i * 5;
+    var x = rad * cos(j);
+    var y = rad * sin(j);
+    var d = dist(0, 0, x, y);
+    var interpColor;
+    if (d <= 200) {
+      interpColor = lerpColor(color(196, 99, 85), color(220, 147, 47), map(d, 0, 200, 0, 1));
+    } else {
+      interpColor = lerpColor(color(220, 147, 47), color(69,106,162), map(d, 200.1, 500, 0, 1));
     }
-    endShape(CLOSE);
+    stroke(interpColor);
+    strokeWeight(2)
+    vertex(x, y);
+    noFill();
   }
+  endShape(CLOSE);
+}
+}
+
+//window resized
+function windowResized() {
+resizeCanvas(windowWidth, windowHeight);
 }
