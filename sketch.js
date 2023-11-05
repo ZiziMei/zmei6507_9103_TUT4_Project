47e6cab6e99db3e3344129b51d02dc2ab3bf8a3f
Zizi Mei <zmei6507@uni.sydney.edu.au>
2023-11-05 20:20:05 +1100

    I updated the sketch

--- sketch.js
+++ sketch.js
@@ -1,17 +1,60 @@
-var angleOff = 10
+let noiseOff = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
+  noFill();
+  strokeWeight(2);
 }
 
 function draw() {
-  angleOff -= 0.2
-  if (angleOff < 10) angleOff = 10
   background(30);
-  rotateX(55);
-  
-  //The Grand Canal
+  rotateX(70);//The perspective
   translate(0, 0, -40);
+  canal();
+  sky();
+}
+
+//Window resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+//The sky
+function sky() {
+  translate(0, 0, 300);
+  rotateX(7);
+
+  for (var i = 0; i < 100; i++) {
+    beginShape();
+    for (var j = 0; j < 360; j += 10) {
+      var rad = i * 8;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var d = dist(0, 0, x, y);
+      var interpColor;
+      //The color of sky
+      if (d <= 200) {
+        interpColor = lerpColor(
+          color(196, 99, 85),
+          color(189, 120, 51),
+          map(d, 0, 200, 0, 1)
+        );
+      } else {
+        interpColor = lerpColor(
+          color(209, 134, 61),
+          color(88, 142, 189),
+          map(d, 200, 500, 0, 1)
+        );
+      }
+      stroke(interpColor);
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+  }
+}
+
+//The Grand Canal
+function canal() {
   for (var i = 0; i < 100; i += 1) {
     beginShape();
     for (var j = 0; j < 360; j += 3) {
@@ -20,45 +63,29 @@ function draw() {
       var y = rad * sin(j);
       var d = dist(0, 0, x, y);
       var interpColor;
-      if (d <= 98) {
-        interpColor = lerpColor(color(196, 99, 85), color(189, 120, 51), map(d, 0, 98, 0, 1));
-      } else {
-        interpColor = lerpColor(color(209, 134, 61), color(88, 142, 189), map(d, 98.1, 200, 0, 1));
-      }
-      stroke(interpColor);
-      strokeWeight(2)
-      vertex(x, y, map(noise(x * 0.01, y * 0.01), 0, 1, -10, 100));
-      noFill();
-    }
-    endShape(CLOSE);
-  }
-  
-  //The sky
-  translate(0, 0, 100);
-  rotateX(7);
-  for (var i = 0; i < 100; i++) {
-    beginShape();
-    for (var j = 0; j < 360; j += angleOff) {
-      var rad = i * 8;
-      var x = rad * cos(j);
-      var y = rad * sin(j);
-      var d = dist(0, 0, x, y);
       var interpColor;
-      if (d <= 66.66) {
-        interpColor = lerpColor(color(196, 99, 85), color(220, 147, 47), map(d, 0, 66.66, 0, 1));
+      if (d <= 200) {
+        interpColor = lerpColor(
+          color(196, 99, 85),
+          color(189, 120, 51),
+          map(d, 0, 200, 0, 1)
+        );
       } else {
-        interpColor = lerpColor(color(220, 147, 47), color(69, 106, 162), map(d, 66.67, 200, 0, 1));
+        interpColor = lerpColor(
+          color(209, 134, 61),
+          color(88, 142, 189),
+          map(d, 200, 500, 0, 1)
+        );
       }
       stroke(interpColor);
-      strokeWeight(2)
-      vertex(x, y);
-      noFill();
+      if (d > 100) {
+        let z = map(noise(x * 0.01, y * 0.01, noiseOff), 0, 1, -10, 100);
+        vertex(x, y, z);
+      }
     }
     endShape(CLOSE);
   }
+  noiseOff += 0.045;
 }
 
-//Window resized
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+
